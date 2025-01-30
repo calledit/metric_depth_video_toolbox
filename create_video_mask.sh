8f1f2fbd2d4b86b0870e4a49d7d6e2e3824b2e9d
Calle <calle@me.me>
2025-01-30 18:05:38 +0100

    fix mask command

--- create_video_mask.sh
+++ create_video_mask.sh
@@ -8,4 +8,4 @@ VIDEO_WIDTH=1440
 VIDEO_HEIGHT=1080
 VIDEO_FPS=25
 
-ffmpeg -i COLOR_VIDEO -ss 1 -an -f rawvideo -pix_fmt rgb24 pipe:1 | rembg b ${VIDEO_WIDTH} ${VIDEO_HEIGHT} | ffmpeg -y -f rawvideo -pix_fmt rgb24 -s ${VIDEO_WIDTH}x${VIDEO_HEIGHT} -r ${VIDEO_FPS} -i pipe:0 -preset fast mask.mp4
+ffmpeg -i ${COLOR_VIDEO} -an -f rawvideo -pix_fmt rgb24 pipe:1 | rembg b ${VIDEO_WIDTH} ${VIDEO_HEIGHT} | ffmpeg -f image2pipe -framerate ${VIDEO_FPS} -vcodec png -i pipe:0 mask.mp4
