257ad40d51424ffe358fc7db65df38feb1bffdc4
calledit <1573053+calledit@users.noreply.github.com>
2025-09-13 00:02:49 +0200

    clean up code

--- unidepth_video.py
+++ unidepth_video.py
@@ -4,6 +4,9 @@ import os
 import torch
 import cv2
 
+import depth_frames_helper
+import depth_map_tools
+
 import sys
 print("please ignore warnings about depreciation warnings for xformers components")
 print("the loading takes a while the first time just wait")
@@ -11,87 +14,6 @@ sys.path.append("UniDepth")
 from unidepth.models import UniDepthV2
 from unidepth.utils import colorize, image_grid
 from unidepth.utils.camera import Pinhole
-
-def compute_camera_matrix(fov_horizontal_deg, fov_vertical_deg, image_width, image_height):
-
-    #We need one or the other
-    if fov_horizontal_deg is not None:
-        # Convert FoV from degrees to radians
-        fov_horizontal_rad = np.deg2rad(fov_horizontal_deg)
-
-        # Compute the focal lengths in pixels
-        fx = image_width /  (2 * np.tan(fov_horizontal_rad / 2))
-
-    if fov_vertical_deg is not None:
-        # Convert FoV from degrees to radians
-        fov_vertical_rad = np.deg2rad(fov_vertical_deg)
-
-        # Compute the focal lengths in pixels
-        fy = image_height /  (2 * np.tan(fov_vertical_rad / 2))
-
-    if fov_vertical_deg is None:
-        fy = fx
-
-    if fov_horizontal_deg is None:
-        fx = fy
-
-    # Assume the principal point is at the image center
-    cx = image_width / 2
-    cy = image_height / 2
-
-    # Construct the camera matrix
-    camera_matrix = np.array([[fx,  0, cx],
-                              [ 0, fy, cy],
-                              [ 0,  0,  1]], dtype=np.float64)
-
-    return camera_matrix
-
-def fov_from_camera_matrix(mat):
-    w = mat[0][2]*2
-    h = mat[1][2]*2
-    fx = mat[0][0]
-    fy = mat[1][1]
-
-    fov_x = np.rad2deg(2 * np.arctan2(w, 2 * fx))
-    fov_y = np.rad2deg(2 * np.arctan2(h, 2 * fy))
-
-    return fov_x, fov_y
-
-def save_24bit(frames, output_video_path, fps, max_depth_arg):
-    """
-    Saves depth maps encoded in the R, G and B channels of a video (to increse accuracy as when compared to gray scale)
-    """
-    height = frames.shape[1]
-    width = frames.shape[2]
-
-    out = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*"FFV1"), fps, (width, height))
-
-    max_depth = frames.max()
-    print("max metric depth: ", max_depth)
-
-    MODEL_maxOUTPUT_depth = max_depth_arg ### pick a value slitght above max metric depth to save the depth in th video file nicly
-    # if you pick a high value you will lose resolution
-
-    # incase you did not pick a absolute value we max out (this mean each video will have depth relative to max_depth)
-    # (if you want to use the video as a depth souce a absolute value is prefrable)
-    if MODEL_maxOUTPUT_depth < max_depth:
-        print("warning: output depth is deeper than max_depth. The depth will be clipped")
-
-    for i in range(frames.shape[0]):
-        depth = frames[i]
-        scaled_depth = (((255**4)/MODEL_maxOUTPUT_depth)*depth.astype(np.float64)).astype(np.uint32)
-
-        # View the depth as raw bytes: shape (H, W, 4)
-        depth_bytes = scaled_depth.view(np.uint8).reshape(height, width, 4)
-
-
-        R = (depth_bytes[:, :, 3]) # Most significant bits in R and G channel (duplicated to reduce compression artifacts)
-        G = (depth_bytes[:, :, 3])
-        B = (depth_bytes[:, :, 2]) # Least significant bit in blue channel
-        bgr24bit = np.dstack((B, G, R))
-        out.write(bgr24bit)
-
-    out.release()
 
 
 if __name__ == '__main__':
@@ -123,7 +45,7 @@ if __name__ == '__main__':
     frame_width, frame_height = int(raw_video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(raw_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
     frame_rate = raw_video.get(cv2.CAP_PROP_FPS)
 
-    cam_matrix = compute_camera_matrix(args.xfov, args.yfov, frame_width, frame_height).astype(np.float32)
+    cam_matrix = depth_map_tools.compute_camera_matrix(args.xfov, args.yfov, frame_width, frame_height)
     cam_matrix_torch = torch.from_numpy(cam_matrix)
 
     model = UniDepthV2.from_pretrained(f"lpiccinelli/unidepth-v2-vitl14").to(DEVICE)
@@ -148,12 +70,11 @@ if __name__ == '__main__':
         predictions = model.infer(rgb_torch, cam_matrix_torch)
         depths.append(predictions["depth"].squeeze().cpu().numpy())
         pred_intrinsic = predictions["intrinsics"].squeeze().cpu().numpy()
-        fovx, fovy = fov_from_camera_matrix(pred_intrinsic)
+        fovx, fovy = depth_map_tools.fov_from_camera_matrix(pred_intrinsic)
         print("fovx:", fovx, "fovy:", fovy)
 
     video_name = os.path.basename(args.color_video)
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
 
-    output_video_path = os.path.join(args.output_dir, os.path.splitext(video_name)[0]+'_depth.mkv')
-    save_24bit(np.array(depths), output_video_path, frame_rate, args.max_depth)
+    depth_frames_helper.save_depth_video(depths, args.color_video+'_depth.mkv', fps, args.max_depth, frame_width, frame_height)
