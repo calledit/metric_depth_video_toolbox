e1d67e341e2c7215a3329c69d25ed35baac6ccaf
calledit <calledit@example.com>
2025-09-26 23:34:37 +0200

    Added depthcrafter and Unik3D to windows install

--- windows_installer.bat
+++ windows_installer.bat
@@ -1,6 +1,11 @@
 echo windows installer for the metric_depth_video_toolbox
 
 set "CONDA=%UserProfile%\miniconda3\condabin\conda.bat"
+
+CALL "%CONDA%" tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+CALL "%CONDA%" tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+CALL "%CONDA%" tos accept --override-channels --channel https://repo.anaconda.com/pkgs/msys2
+
 CALL "%CONDA%"  create -n mdvt python=3.11 -y
 CALL "%UserProfile%\miniconda3\Scripts\activate.bat" mdvt
 pip install numpy open3d opencv-python
@@ -72,3 +77,12 @@ echo infill model installed
 echo install masking dependencies
 pip install rembg[gpu] filetype watchdog aiohttp asyncer gradio
 CALL "%CONDA%"  install -c conda-forge cudnn=9.*
+
+echo install DepthCrafter model
+git clone https://github.com/Tencent/DepthCrafter
+pip install mediapy decord
+
+echo install Unikd3D model
+git clone https://github.com/lpiccinelli-eth/UniK3D
+
+pip install timm wandb
