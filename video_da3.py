c4ea794248dcec3989def06ea9adc676149f4df3
calledit <calledit@example.com>
2025-11-14 21:41:46 +0100

    batched output of video depth anything v3

--- video_da3.py
+++ video_da3.py
@@ -3,6 +3,7 @@ import numpy as np
 import sys
 sys.path.append("Depth-Anything-3/src")
 from depth_anything_3.api import DepthAnything3
+from depth_anything_3.utils.pose_align import align_poses_umeyama, _apply_sim3_to_poses
 import cv2
 import depth_frames_helper
 import depth_map_tools
@@ -17,6 +18,35 @@ class NumpyEncoder(json.JSONEncoder):
             return obj.item()
         return super().default(obj)
 
+model = None
+
+def do_batch(list_of_ids, intrinsics, extrinsics, images):
+    
+    picked_imgs = []
+    picked_extrs = []
+    picked_intrs = []
+
+    for i in list_of_ids:
+        picked_imgs.append(images[i])
+        if extrinsics is not None:
+            picked_imgs.append(extrinsics[i])
+        if intrinsics is not None:
+            picked_imgs.append(intrinsics[i])
+    
+    if extrinsics is None:
+        picked_extrs = None
+    
+    if intrinsics is None:
+        picked_intrs = None
+        
+    prediction = model.inference(
+        picked_imgs,
+        extrinsics = picked_extrs,
+        intrinsics = picked_intrs,
+    )
+    return prediction
+
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MDVT UniK3D video converter')
     parser.add_argument('--color_video', type=str, required=True)
@@ -27,7 +57,10 @@ if __name__ == '__main__':
     parser.add_argument('--yfov', type=float, help='fov in deg in the y-direction, calculated from aspectratio and xfov in not given', required=False)
     parser.add_argument("--transformation_file", type=str, default=None)
     parser.add_argument('--xfov_file', type=str, help='alternative to xfov and yfov, json file with one xfov for each frame', required=False)
-
+    parser.add_argument('--images_per_batch', default=30, type=int, help='nr of images per batch set depening on GPU memmory', required=False)
+    parser.add_argument('--batch_overlap', default=6, type=int, help='nr of images that is overlaped from the last batch', required=False)
+    parser.add_argument('--nr_of_ref_frames', default=6, type=int, help='nr of references that will be picked ', required=False)
+    
     args = parser.parse_args()
 
 
@@ -74,7 +107,7 @@ if __name__ == '__main__':
             xfov = xfovs[i]
             cam_matrix = compute_camera_matrix(xfov, None, original_width, original_height).astype(np.float32)
             intrinsics.append(cam_matrix)
-      
+    
     
     if args.transformation_file is not None and intrinsics is not None:
         if not os.path.isfile(args.transformation_file):
@@ -87,24 +120,88 @@ if __name__ == '__main__':
            
      
     
+    # select reference frames
+    nth_frame = nr_images//args.nr_of_ref_frames
+    
+    reference_frame_ids = []
+    for i in range(nr_images):
+        if i % nth_frame == 0:
+            reference_frame_ids.append(i)
+    numer_of_refs = len(reference_frame_ids)
+    
+    args.images_per_batch -= args.nr_of_ref_frames+args.batch_overlap
+    
+    batches = []
+    for i in range(nr_images):
+        if i % args.images_per_batch == 0:
+            batches.append([])
+        batches[-1].append(i)
+    
+    
     
     # Load model from Hugging Face Hub
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = DepthAnything3.from_pretrained("depth-anything/da3nested-giant-large")
     model = model.to(device=device)
     
+    depth_out = []
+    intrin_out = []
+    extrin_out = []
     
-    prediction = model.inference(
-        images,
-        extrinsics = extrinsics,
-        intrinsics = intrinsics,
-    )
+    alingnment_extrinsics = None
+    last_frame = None
+    last_frame_tranform = None
+    for batch in batches:
+        nr_in_batch = len(batch)
+        if nr_in_batch != 0:
+            to_batch = reference_frame_ids + []
+            
+            nr_used_refs = numer_of_refs
+            ##add last_frame for the last batch as a ref frame
+            if last_frame is not None:
+                to_batch.extend(last_frame)
+                nr_used_refs = len(to_batch)
+                
+            to_batch.extend(batch)
+            prediction = do_batch(to_batch, intrinsics, extrinsics, images)
+            
+            
+            ref_extrinsics = prediction.extrinsics[:nr_used_refs]
+            
+            
+            
+            if alingnment_extrinsics is None:
+                alingnment_extrinsics = ref_extrinsics
+            
+            batch_alignment_extrinsics = alingnment_extrinsics
+            if last_frame_tranform is not None:
+                batch_alignment_extrinsics = np.concatenate([alingnment_extrinsics, np.array(last_frame_tranform)], axis=0)
+                
+            r, t, s = align_poses_umeyama(batch_alignment_extrinsics, ref_extrinsics, return_aligned=False)
+            
+            aligned_extrinsics = _apply_sim3_to_poses( prediction.extrinsics[nr_used_refs:], r, t, s)
+            
+            #print(aligned_extrinsics)
+                
+            # Align acording to old extrinsics
+            prediction.extrinsics[:nr_used_refs]
+            #Mabye we should align the depth to?
+            #prediction.depth[:nr_used_refs]
+            
+            #We skip the reference frames
+            depth_out.extend(prediction.depth[nr_used_refs:])
+            intrin_out.extend(prediction.intrinsics[nr_used_refs:])
+            extrin_out.extend(aligned_extrinsics)
+        
+            last_frame_tranform = aligned_extrinsics[-args.batch_overlap:]
+            last_frame = batch[-args.batch_overlap:]
+        
 
-    depth_frames_helper.save_depth_video(prediction.depth, output_tmp_video_path, fps, args.max_depth, W, H)
-    depth_frames_helper.verify_and_move(output_tmp_video_path, len(prediction.depth), output_video_path)
+    depth_frames_helper.save_depth_video(depth_out, output_tmp_video_path, fps, args.max_depth, W, H)
+    depth_frames_helper.verify_and_move(output_tmp_video_path, len(depth_out), output_video_path)
 
     out_xfovs = []
-    for intrin in prediction.intrinsics:
+    for intrin in intrin_out:
         fovx, fovy = depth_map_tools.fov_from_camera_matrix(intrin)
         out_xfovs.append(float(fovx))
         
@@ -112,7 +209,7 @@ if __name__ == '__main__':
         json_file_handle.write(json.dumps(out_xfovs))
     
     out_transformations = []
-    for extrin in prediction.extrinsics:
+    for extrin in extrin_out:
         fixed_extrin = np.vstack([extrin, np.array([0, 0, 0, 1], dtype=extrin.dtype)])
         fixed_extrin = np.linalg.inv(fixed_extrin)#da3 outputs inverted transformations to waht we want
         out_transformations.append(fixed_extrin)
@@ -122,8 +219,8 @@ if __name__ == '__main__':
     
     
     #DEBUG grayscale out
-    max_depth = np.max(prediction.depth)
-    depth_frames_helper.save_grayscale_video(prediction.depth, output_video_path+"_grayscale_depth.mkv", fps, max_depth, W, H) # prediction.depth.shape[2], prediction.depth.shape[1]
+    max_depth = np.max(depth_out)
+    depth_frames_helper.save_grayscale_video(depth_out, output_video_path+"_grayscale_depth.mkv", fps, max_depth, W, H) # prediction.depth.shape[2], prediction.depth.shape[1]
     
     
     # Access results
